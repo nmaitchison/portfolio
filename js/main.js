7a711f206bfedce712ea857a7d3b0a1e91b385c0
nlaitchison <nlaitchison@gmail.com>
2016-06-05 12:30:17 -0400

    projects are added dynamically

--- js/main.js
+++ js/main.js
@@ -24,11 +24,31 @@ $(function(){
 	// 	}
 	// );
 	
-	var projects = [
-		{
+var projects = [
+		
+{
+			"id" : "TFH",
+			"name" : "The Finest Hours",
+			"type" : "VFX Work",
+			"skills" : ["Lighting"], 
+			"description" : "These are screencaps of some of the shots I worked on for Disney’s <i>The Finest Hours</i>. I did lighting for the Pendleton Extensions and the generic CG water shots for the rescue sequence.",
+			"preview" : "images/TFH/TFH_cover.jpg",
+			"images" : ["images/TFH/MPC_VFX_O_CH0043_v32_still.jpg","images/TFH/MPC_VFX_O_CH0806_v160_still.jpg","images/TFH/TEX/MPC_VFX_O_CM0670_v53_still.jpg","images/TFH/MPC_VFX_O_PR0180_v37_still.jpg","images/TFH/TEX/MPC_VFX_O_PR0860_v62_still.jpg"]
+		},
+{
+			"id" : "F4",
+			"name" : "Fantastic Four",
+			"type" : "VFX Work",
+			"skills" : ["Lighting"], 
+			"description" : "These are screencaps of some of the shots I worked on for Marvel’s <i>Fantastic Four</i>. I did lighting for The Thing and the Planet Zero environment for the final battle sequence.",
+			"preview" : "images/F4/F4_cover.jpg",
+			"images" : ["images/F4/MPC_VFX_FF0112_still.jpg","images/F4/MPC_VFX_FF0614_still.jpg","images/F4/TEX/MPC_VFX_FF0616_still.jpg","images/F4/TEX/MPC_VFX_FF0614_still.jpg","images/F4/TEX/MPC_VFX_FF1076_still.jpg"]
+		},
+{
 			"id" : "PCC1",
 			"name" : "Mangos - Demo Reel",
 			"type" : "Demo Reel",
+"skills" : ["Modeling","Textures", "Lighting &amp; Shading"], 
 			"description" : "This is the second scene in my demo reel. I modeled everything in the scene, lit the scene and made all the textures and materials.",
 			"preview" : "images/PCC1/PCC1-preview.jpg",
 			"images" : ["images/PCC1/PCC1_render.jpg","images/PCC1/PCC1_wireframe.jpg","images/PCC1/TEX/mango1_UV_BASE.jpg","images/PCC1/TEX/mango2_UV_BASE.jpg","images/PCC1/TEX/mango3_UV_BASE.jpg","images/PCC1/TEX/vase_UV_BASE.jpg"]
@@ -37,6 +57,7 @@ $(function(){
 			"id" : "PCC2",
 			"name" : "Thread - Demo Reel",
 			"type" : "Demo Reel",
+"skills" : ["Modeling","Textures", "Lighting &amp; Shading"], 
 			"description" : "This is the third scene in my demo reel. I modeled everything in the scene, lit the scene and made all the textures and materials.",
 			"preview" : "images/PCC2/PCC2-preview.jpg",
 			"images" : ["images/PCC2/PCC2_render.jpg","images/PCC2/PCC2_wireframe.jpg","images/PCC2/TEX/label2_UV.jpg","images/PCC2/TEX/label3_UV.jpg","images/PCC2/TEX/label4_UV.jpg","images/PCC2/TEX/label5_UV.jpg","images/PCC2/TEX/spool1_UV.jpg","images/PCC2/TEX/spool2_UV.jpg","images/PCC2/TEX/spool3_UV.jpg","images/PCC2/TEX/spool4_UV.jpg","images/PCC2/TEX/spool5_UV.jpg","images/PCC2/TEX/table_UV.jpg"]
@@ -45,6 +66,7 @@ $(function(){
 			"id" : "PCC3",
 			"name" : "Kitchen - Demo Reel",
 			"type" : "Demo Reel",
+			"skills" : ["Modeling","Textures", "Lighting &amp; Shading"], 
 			"description" : "This is the first scene in my demo reel. I modeled everything in the scene, lit the scene and made all the textures and materials.",
 			"preview" : "images/PCC3/PCC3-preview.jpg",
 			"images" : ["images/PCC3/PCC3_render.jpg","images/PCC3/PCC3_wireframe.jpg","images/PCC3/TEX/lamp_REFL.jpg"]
@@ -53,6 +75,7 @@ $(function(){
 			"id" : "hallway",
 			"name" : "Hallway - Classwork",
 			"type" : "Class Project",
+			"skills" : ["Lighting &amp; Shading"], 
 			"description" : "This is an assignment I did in my Visual Development class. I had to match the lighting from a reference image using the techniques taught in the class. Mine is on the left and the reference is on the right.",
 			"preview" : "images/hallway/hallway-preview.jpg",
 			"images" : ["images/hallway/HALLWAY.jpg"]
@@ -61,22 +84,24 @@ $(function(){
 			"id" : "jar",
 			"name" : "Jar - Classwork",
 			"type" : "Class Project",
+			"skills" : ["Textures","Lighting &amp; Shading"], 
 			"description" : "This is an assignment I did in my Shading and Lighting class. I had to replicate the textures and lighting based on a real life reference image",
-			"preview" : "mages/jar/jar-preview.jpg",
+			"preview" : "images/jar/jar-preview.jpg",
 			"images" : ["images/jar/JAR.jpg"]
 		},
 			{
 			"id" : "crush",
 			"name" : "Crush - Classwork",
 			"type" : "Class Project",
+			"skills" : ["Textures","Lighting &amp; Shading"], 
 			"description" : "This is an assignment I did in my Visual Development class. I had to make the materials for the bottle and make the uvs and materials for the gear. I also had to add a sticker and water stains to the box using projections.",
 			"preview" : "images/crush/crush-preview.jpg",
 			"images" : ["images/crush/CRUSH.tga"]
-		},
-			{
+		},			{
 			"id" : "cheese",
 			"name" : "Cheese - Classwork ",
 			"type" : "Class Project",
+"skills" : ["Textures","Lighting &amp; Shading"], 
 			"description" : "This is an assignment I did in my Shading and Lighting class. I had do the materials and textures for the cheese, knife, cutting board, wine bottle and wine glass. Everything else in the scene was given to me already finished.",
 			"preview" : "images/cheese/cheese-preview.jpg",
 			"images" : ["images/cheese/CHEESE.jpg"]
@@ -85,30 +110,54 @@ $(function(){
 			"id" : "clothing-dev",
 			"name" : "Clothing Development",
 			"type" : "Class Project",
+"skills" : ["Character Development"], 
 			"description" : "This is an assignment I did in my Character Development class. I had to create a reference sheet similar to something I would show an art director for the clothing of a character I created. I had to base the character of a time period, clothing style and job given from a random generator. I got cyberpunk graffiti artist from the present.",
-			"preview" : "images/clothing-dev/ClothingDevelopment.jpg",
+			"preview" : "images/clothing-dev/clothing-dev-preview.jpg",
 			"images" : ["images/clothing-dev/ClothingDevelopment.jpg"]
 		},
 		{
 			"id" : "emma-waffles",
 			"name" : "Emma & Waffles",
 			"type" : "Class Project",
+"skills" : ["Character Development"], 
 			"description" : "This is an assignment I did in my Methods of Design class. I had to create  2 characters, a hero and sidekick, for a fake tv show based off of a random target audience and object given to me. I was given the age group 4-7 and cake.",
 			"preview" : "images/emma-waffles/emma-waffles-preview.jpg",
 			"images" : ["images/emma-waffles/doodle_emma.jpg","images/emma-waffles/emmaWaffles_2.jpg","images/emma-waffles/emmaWaffles_1.jpg","images/emma-waffles/emma.jpg","images/emma-waffles/waffles.jpg"]
 		}, 
+{
+			"id" : "bellaluna",
+			"name" : "Bellaluna",
+			"type" : "Logo Design",
+			"skills" : ["Logo Design"], 
+			"description" : "I designed this logo for my moms hand made cards and jewlery shop called Bellaluna.",
+			"preview" : "images/bella/BELLALUNA_cover.jpg",
+			"images" : ["images/bella/bellaluna_COLOR.png","images/bella/bellaluna_BW.png"]
+		},
+
 		{
 			"id" : "cougar-knits",
 			"name" : "Cougar Knits",
 			"type" : "Logo Design",
+			"skills" : ["Logo Design"], 
 			"description" : "I designed this logo for my friend’s organization called Cougar Knits.",
 			"preview" : "images/cougar-knits/cougar-knits-preview.jpg",
 			"images" : ["images/cougar-knits/cougar_knits.jpg"]
 		},
+{
+			"id" : "WZL",
+			"name" : "Wenzel Downhole Tools Shirt",
+			"type" : "Shirt Design",
+			"skills" : ["Shirt Design"], 
+			"description" : "I made this design for Wenzel Downhole Tools.",
+			"preview" : "images/WZL/WZLshirt_cover.jpg",
+			"images" : ["images/WZL/wzl_mockup.jpg"]
+		},
+
 		{
 			"id" : "molly",
 			"name" : "Robot - Akëro Clothing",
 			"type" : "Shirt Design",
+			"skills" : ["Shirt Design"], 
 			"description" : "I made this design for my clothing company Akëro Clothing.",
 			"preview" : "images/molly/molly-preview.jpg",
 			"images" : ["images/molly/molly_main.png"]
@@ -117,19 +166,36 @@ $(function(){
 			"id" : "cat",
 			"name" : "Cat - Akëro Clothing",
 			"type" : "Shirt Design",
+			"skills" : ["Shirt Design"], 
 			"description" : "I made this design for my clothing company Akëro Clothing.",
 			"preview" : "images/cat/cat-preview.jpg",
 			"images" : ["images/cat/cat_main.png"]
 		} 
 	];
 	
+	var max = projects.length;
 	
-	function setWork(obj){
+	function setWork(){
+		var html = '';
+		for(var i = 0; i < max; i++){
+			var obj = projects[i],
+				skills = '';
+			for(var j = 0; j < obj.skills.length; j++){
+				skills += '<li><span class="bullet">+</span>'+obj['skills'][j]+'</li>';	
+			}
+			html += '<div class="four columns"><div class="item" id="'+obj.id+'"><div class="overlay"><h3>'+obj.type+'</h3><ul>'+skills+'</ul></div><img src="'+obj.preview+'"></div></div>';
+		}
+		$('#work .work-container').prepend(html);
+	}	
+	
+	setWork();
+	
+	function setDetails(obj){
 		var imgs = '';
 		for(var j = 0; j < obj['images'].length; j++){
 			imgs += '<img src="'+obj['images'][j]+'">';
 		}
-		var html = '<div id="work-desc" class="seven columns"><a href="#work" id="back">« Back to Works</a><h2>'+obj.name+'</h2><p>'+obj.description+'</p></div><div id="work-img" class="five columns">'+imgs+'</div><div class="clear-fix"></div>';
+		var html = '<div id="work-desc" class="seven columns"><a href="#work" id="back">« Back to My Work</a><h2>'+obj.name+'</h2><p>'+obj.description+'</p></div><div id="work-img" class="five columns">'+imgs+'</div><div class="clear-fix"></div>';
 		$("#work-details").append(html).show();
 		$("#main-container").hide();
 		window.scrollTo(0, 0);
@@ -141,13 +207,11 @@ $(function(){
 		$("#work-details").hide();
 	});
 	
-	var max = projects.length;
-	
 	$(".work-container .item").on('click',function(e) {
 		$("#work-details").empty();
 		for(var i = 0; i < max; i++){
 			if(projects[i]['id'] == this.id){
-				setWork(projects[i]);
+				setDetails(projects[i]);
 				break;
 			}
 		}
